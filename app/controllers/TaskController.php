e61e3d123e8c0d16d45a5b4d615a35fa5e459546
m4arthu <luisarthir259@gmail.com>
2023-11-22 15:43:41 -0300

    fix: comments in ./controllers

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -6,29 +6,31 @@ use app\repositorys\TaskRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+// controller  chamado nas rotas
+// gerencia oas  respostas  e coleta  os parametros e o corpo  da requisição passando  para o  repository adequado
 class TaskController
 {
 
     public function index(Request $request, Response $response)
     {
-        $tasks = TaskRepository::getTasks();
-        $body = $response->getBody();
-        $body->write(json_encode($tasks));
+        $tasks = TaskRepository::getTasks(); // pega as tarefas no banco de dados
+        $body = $response->getBody(); // coleta  o  corpo da requisiçãoi
+        $body->write(json_encode($tasks)); // transforma em  json
         return $response;
     }
 
     public function store(Request $request, Response $response)
     {
         $taskData = $request->getParsedBody();
-        $bodyValidation = TaskModels::postModel($taskData);
+        $bodyValidation = TaskModels::postModel($taskData); // valida o body da requisição
 
         if ($bodyValidation !== true) {
-            $body = $response->getBody();
+            $body = $response->getBody(); 
             $body->write($bodyValidation);
             return $response->withStatus(422);
         }
 
-        $task = taskRepository::createTask($taskData);
+        $task = taskRepository::createTask($taskData); // cria  a  task  no banco  de dados
         $body = $response->getBody();
         if($task === true){
             $body->write("Task Created successfully");
@@ -49,7 +51,7 @@ class TaskController
             return $response->withStatus(422);
         }
 
-        $task = taskRepository::updateTask($taskData);
+        $task = taskRepository::updateTask($taskData); // atualiza a task 
         $body = $response->getBody();
         if($task === true){
             $body->write("Task Updated successfully");
@@ -65,7 +67,7 @@ class TaskController
     public function destroy(Request $request, Response $response,$args){
         $taskId = $args['id'];
 
-        $task = taskRepository::deleteTask($taskId);
+        $task = taskRepository::deleteTask($taskId); // deleta  a task
         $body = $response->getBody();
         if($task === true){
             $body->write("Task deleted successfully");
