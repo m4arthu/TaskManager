98c08c9ba44d2ebb554d900fecd7a2d8c78ee193
m4arthu <luisarthir259@gmail.com>
2023-11-22 15:27:52 -0300

    fix: comments in  public/index.php

--- public/index.php
+++ public/index.php
@@ -2,16 +2,21 @@
 
 use Slim\Factory\AppFactory;
 require '../vendor/autoload.php';
-
+ 
+/// criando um  app  com a classe slim
 $app = AppFactory::create();
 
+// carregando  as variaveis  de ambiente
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// para passar o  corpo da requisição em formato  json
 $app->addBodyParsingMiddleware();
 
+//carregando  as rotas do app
 $routes = require '../app/routes/routes.php';
 
 $routes($app);
 
+// iniciando Api
 $app->run();
