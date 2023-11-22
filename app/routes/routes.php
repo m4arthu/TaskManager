9cfd843d48908a20139f5d43963cf16302481be8
m4arthu <luisarthir259@gmail.com>
2023-11-22 15:29:35 -0300

    fix: comments in ./routes

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -4,7 +4,7 @@ use Slim\App;
 
 use app\controllers\TaskController;
 
-
+// configurando  as rotas e os endpoinds de cada uma delas  usando  controllers
 return function (App $app) {
     $app->get('/tasks', [TaskController::class, 'index']);
     $app->post('/tasks', [TaskController::class, 'store']);
