a788d2282d96b610a32ff5921da2c69e75720dd5
m4arthu <luisarthir259@gmail.com>
2023-11-23 21:12:49 -0300

    fix: CORS form  methods OPTION alowed

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -11,14 +11,16 @@ use Slim\Routing\RouteContext;
 // configurando  as rotas e os endpoinds de cada uma delas  usando  controllers
 return function (App $app) {
     // perimitindo cors origin
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
     $app->add(function (Request $request, RequestHandlerInterface $handler): Response {
         $routeContext = RouteContext::fromRequest($request);
         $routingResults = $routeContext->getRoutingResults();
         $methods = $routingResults->getAllowedMethods();
         $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
-    
         $response = $handler->handle($request);
-    
         $response = $response->withHeader('Access-Control-Allow-Origin', '*');
         $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
         $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
