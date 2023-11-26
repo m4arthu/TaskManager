664ab8413a25d5f7de87d9c92b74d0468407e096
m4arthu <luisarthir259@gmail.com>
2023-11-26 15:59:53 -0300

    feat: docker-compose added to  link all  containers

--- app/database/Connection.php
+++ app/database/Connection.php
@@ -12,7 +12,7 @@ class Connection
     public static function getConnection()
     {
         if (!self::$connect) {
-            self::$connect = new PDO("mysql:host=localhost;dbname={$_ENV['DATABASE_NAME']}", $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD'], [
+            self::$connect = new PDO("mysql:host=mysql;dbname={$_ENV['DATABASE_NAME']}", $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD'], [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
             ]);
         }
