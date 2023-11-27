de54d503c5748d6decb7dc9875ce059b415fe807
m4arthu <luisarthir259@gmail.com>
2023-11-26 21:30:07 -0300

    fix: dumper container not  working

--- docker/dumper/dump.sql
+++ docker/dumper/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Nov-2023 às 01:47
+-- Tempo de geração: 27-Nov-2023 às 00:38
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -11,36 +11,45 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS taskmanagerapi;  
- USE taskmanagerapi; 
+CREATE DATABASE IF NOT EXISTS taskmanagerapi;
 
+USE taskmanagerapi; -- Adicione esta linha para selecionar o banco de dados
+
+-- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `tbl_tasks`
 --
+DROP TABLE IF EXISTS `tbl_tasks`;
 
 CREATE TABLE `tbl_tasks` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(255) DEFAULT NULL,
   `task_description` varchar(255) DEFAULT NULL,
-  `deadline` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `deadline` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `tbl_tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(255) DEFAULT NULL,
+  `task_description` varchar(255) DEFAULT NULL,
+  `deadline` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tabelas despejadas
+-- Extraindo dados da tabela `tbl_tasks`
 --
 
---
--- Índices para tabela `tbl_tasks`
---
-ALTER TABLE `tbl_tasks`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `tbl_tasks` (`task_name`, `task_description`, `deadline`) VALUES
+('dwadawdaw', 'dawdawd', '0000-00-00 00:00:00');
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -50,7 +59,7 @@ ALTER TABLE `tbl_tasks`
 -- AUTO_INCREMENT de tabela `tbl_tasks`
 --
 ALTER TABLE `tbl_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
